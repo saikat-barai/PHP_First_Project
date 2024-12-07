6eae8651967a2d1716114bf049f7788ad65a3a34
Saikat Barai <saikatbarai299@.com>
2024-12-07 22:08:12 +0600

    number_classifier

---
+++ 1_number_classifier.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Enter a number: ";
+$number = (int)readline();
+
+if ($number > 0) {
+    echo "The number is positive.";
+}
+elseif ($number < 0) {
+    echo "The number is negative.";
+}
+else {
+    echo "The number is zero.";
+}
